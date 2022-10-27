b42ef3438e5886183194b466e0586c121b19860a
licongcong <1098125932@qq.com>
2022-10-28 00:54:48 +0800

    update first-add-plus-note

--- promise.js
+++ promise.js
@@ -10,73 +10,85 @@ function isObject(obj) {
     return Object.prototype.toString.call(obj).toLowerCase() === "[object object]";
 }
 class Container {
-    static resolve = (data) => {
-        if (this.status === PENDING) {
-            this.status = FULFILLED;
-            this.value = data;
-            while (this.onResolvedCallbacks.length > 0) {
-                this.onResolvedCallbacks.shift()(data);
-            }
-        }
-    }
-    static reject = (reason) => {
-        if (this.status === FULFILLED) {
-            this.status = FULFILLED;
-            this.reason = reason;
-            while (this.onRejectedCallbacks.length > 0) {
-                this.onRejectedCallbacks.shift()(reason);
-            }
-        }
-    }
     constructor(executor) {
         this.status = PENDING;  // 初始状态
         this.value = null;      // promise成功的值
         this.reason = null;     // promise失败原因
         this.onResolvedCallbacks = [];  // then->resolve时的回调函数集合，在promise成功之前可能会有多个resolve
         this.onRejectedCallbacks = [];  // then->rejected时的回调函数集合，在promise成功之前可能会有多个rejected
+        let resolve = (data) => {
+            setTimeout(() => {
+                if (this.status === PENDING) {
+                    this.status = FULFILLED;
+                    this.value = data;
+                    while (this.onResolvedCallbacks.length > 0) {
+                        this.onResolvedCallbacks.shift()(data);
+                    }
+                }
+            })
+        }
+        let reject = (reason) => {
+            setTimeout(() => {
+                if (this.status === PENDING) {
+                    this.status = REJECTED;
+                    this.reason = reason;
+                    while (this.onRejectedCallbacks.length > 0) {
+                        this.onRejectedCallbacks.shift()(reason);
+                    }
+                }
+            })
+        }
         try {
-            executor(this.resolve, this.reject);
+            executor(resolve, reject);
         } catch (error) {
-            this.reject(error);
+            reject(error);
         }
     }
     then (onResolved, onRejected) {
-        onResolved = isFunction(onResolved) ? onResolved : (data) => data;
-        onRejected = isFunction(onRejected) ? onRejected : err => {throw err};
+        onResolved = isFunction(onResolved) ? onResolved : function (data) {return data};
+        onRejected = isFunction(onRejected) ? onRejected : function (error) {throw error};
         let promise2 = new Container((resolve, reject) => {
             switch (this.status) {
                 case PENDING:
                     this.onResolvedCallbacks.push(() => {
-                        let x = onResolved(this.value);
-                        if (x instanceof Container) {
-                            x.then(this.value);
-                        }
-                        resolve(x);
+                        setTimeout(() => {
+                            try {
+                                let x = onResolved(this.value);
+                                resolvePromise(promise2, x, resolve, reject);
+                            } catch (error) {
+                                reject(error)
+                            }
+                        })
                     })
                     this.onRejectedCallbacks.push(() => {
-                        let x = onRejected(this.reason);
-                        if (x instanceof Container) {
-                            x.then(this.reason);
-                        }
-                        reject(x);
+                        setTimeout(() => {
+                            try {
+                                let x = onRejected(this.reason);
+                                resolvePromise(promise2, x, resolve, reject);
+                            } catch (error) {
+                                reject(error);
+                            }
+                        })
                     })
                     break;
                 case FULFILLED:
-                    this.onResolvedCallbacks.push(() => {
-                        let x = onResolved(this.value);
-                        if (x instanceof Container) {
-                            x.then(this.value);
+                    setTimeout(() => {
+                        try {
+                            let x = onResolved(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (error) {
+                            reject(error)
                         }
-                        resolve(x);
                     })
                     break;
                 case REJECTED:
-                    this.onRejectedCallbacks.push(() => {
-                        let x = onRejected(this.reason);
-                        if (x instanceof Container) {
-                            x.then(this.reason);
+                    setTimeout(() => {
+                        try {
+                            let x = onRejected(this.reason);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (error) {
+                            reject(error);
                         }
-                        reject(x);
                     })
                     break;
                 default: break;
@@ -85,7 +97,77 @@ class Container {
         return promise2;
     }
 }
-Container.defer = function () {
+// function resolvePromise(promise2, x, resolve, reject) {
+//     if (x === promise2) {
+//         return reject(new TypeError('Chaining cycle detected for promise'));
+//     }
+//     let called;
+//     if (x != null && (isObject(x) || isFunction(x))) {
+//         try {
+//             let then = x.then;
+//             if (isFunction(then)) {
+//                 then.call(x, (data) => {
+//                     if (called) return;
+//                     called = true;
+//                     resolvePromise(promise2, data, resolve, reject)
+//                 }, (error) => {
+//                     if (called) return;
+//                     called = true;
+//                     reject(error);
+//                 })
+//             } else {
+//                 resolve(x);
+//             }
+//         } catch (error) {
+//             if (called) return;
+//             called = true;
+//             reject(error);
+//         }
+//     } else {
+//         resolve(x);
+//     }
+// }
+const resolvePromise = (promise2, x, resolve, reject) => {
+    // 自己等待自己完成是错误的实现，用一个类型错误，结束掉 promise  Promise/A+ 2.3.1
+    if (promise2 === x) {
+        return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+    }
+    // Promise/A+ 2.3.3.3.3 只能调用一次
+    let called;
+    // 后续的条件要严格判断 保证代码能和别的库一起使用
+    if ((typeof x === 'object' && x != null) || typeof x === 'function') {
+        try {
+            // 为了判断 resolve 过的就不用再 reject 了（比如 reject 和 resolve 同时调用的时候）  Promise/A+ 2.3.3.1
+            let then = x.then;
+            if (typeof then === 'function') {
+                // 不要写成 x.then，直接 then.call 就可以了 因为 x.then 会再次取值，Object.defineProperty  Promise/A+ 2.3.3.3
+                then.call(x, y => { // 根据 promise 的状态决定是成功还是失败
+                    if (called) return;
+                    called = true;
+                    // 递归解析的过程（因为可能 promise 中还有 promise） Promise/A+ 2.3.3.3.1
+                    resolvePromise(promise2, y, resolve, reject);
+                }, r => {
+                    // 只要失败就失败 Promise/A+ 2.3.3.3.2
+                    if (called) return;
+                    called = true;
+                    reject(r);
+                });
+            } else {
+                // 如果 x.then 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.3.4
+                resolve(x);
+            }
+        } catch (e) {
+            // Promise/A+ 2.3.3.2
+            if (called) return;
+            called = true;
+            reject(e)
+        }
+    } else {
+        // 如果 x 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.4  
+        resolve(x)
+    }
+}
+Container.deferred = Container.defer = function () {
     let dfd = {};
     dfd.promise = new Container((resolve, reject) => {
         dfd.resolve = resolve;
